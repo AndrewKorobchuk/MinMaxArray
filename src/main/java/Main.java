97b576e271357a28a1c59f2f0cc79a16da87961f
Andrew <sherlockh989@gmail.com>
2023-01-31 13:24:03 +0200

    Commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int[] minMax = MinMax.minMax(arr);
+        System.out.println("Min: " + minMax[0] + "; Max: " + minMax[1]);
+    }
 }
--- src/main/java/MinMax.java
+++ src/main/java/MinMax.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class MinMax {
+import java.util.Arrays;
+
+public class MinMax {
+    public static int[] minMax(int[] arr){
+        int[] temp = arr.clone();
+        Arrays.sort(temp);
+        int[] m = new int[2];
+        m[0] = temp[0];
+        m[1] = temp[temp.length-1];
+        return m;
+    }
 }
